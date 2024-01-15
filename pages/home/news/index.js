8dacd8d3b3b9acfb5bdbff032ebea747bb14da17
tanmaysihare <tanmaysihare@gmail.com>
2024-01-15 13:46:08 +0530

    final touch to about us page(news page)

--- pages/home/news/index.js
+++ pages/home/news/index.js
@@ -1,9 +1,14 @@
-
+import Link from "next/link"
 
 function news() {
   return (
     <div>
-      <h3>this is news </h3>
+      <div><Link href='/home/news/1'>Yash</Link></div>
+      <div><Link href='/home/news/2'>Vaibhav</Link></div>
+      <div><Link href='/home/news/3'>Suresh</Link></div>
+      
+      
+      
     </div>
   )
 }
